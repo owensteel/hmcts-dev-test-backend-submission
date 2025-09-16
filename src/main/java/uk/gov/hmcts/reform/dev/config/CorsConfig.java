4a103bc83d6d2b432682338e4238bf3ccc9b395b
Owen A Steel <woofdog04@gmail.com>
2025-09-16 16:18:41 +0100

    Adds CORS config

---
+++ src/main/java/uk/gov/hmcts/reform/dev/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.dev.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://localhost:3100")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE");
+    }
+}
