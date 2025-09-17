d8812ee753bade10519fe7264ae4b2f6ec72d4c2
Owen A Steel <woofdog04@gmail.com>
2025-09-17 11:26:37 +0100

    Setup for Postgresql and DB

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,12 @@
 CREATE TABLE
-    tasks (
+    IF NOT EXISTS tasks (
         id BIGSERIAL PRIMARY KEY,
         title VARCHAR(120) NOT NULL,
         description TEXT,
         status VARCHAR(20) NOT NULL,
         due_at TIMESTAMPTZ NOT NULL
     );
+
+CREATE INDEX IF NOT EXISTS idx_tasks_status ON tasks (status);
+
+CREATE INDEX IF NOT EXISTS idx_tasks_due_at ON tasks (due_at);
